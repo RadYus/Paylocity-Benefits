cf389037ea54f5fa068fe048d01ce7eaa458db18
RadYus <rambajka@seznam.cz>
2026-03-28 21:15:15 +0100

    fix: be test

--- automation/be/beTest.spec.js
+++ automation/be/beTest.spec.js
@@ -4,31 +4,20 @@ test.describe.serial('REST API', () => {
 	let api;
 
 	test.beforeAll(async ({ playwright }) => {
-		api = await playwright.request.newContext({baseURL: 'http://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod'});
+		api = await playwright.request.newContext({baseURL: 'https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com'});
 	});
 
 
 	test.afterAll(async () => {
 		if (api) await api.dispose();
 	});
-	test('GET with Basic Auth header', async () => {
+	test('GET list of employees', async () => {
         const AUTHENTICATION = process.env.BENEFITS_AUTHENTICATION;
 		const basic = `${AUTHENTICATION}`;
-		const response = await api.get('/api/Employees', {
-    headers: {
-    Authorization: `Basic ${basic}`,
-    Accept: '*/*',
-    'User-Agent': 'PostmanRuntime/7.32.3',
-    Host: 'wmxrwq14uc.execute-api.us-east-1.amazonaws.com'
-  }
-});
+		const response = await api.get('/Prod/api/Employees', {headers: {Authorization: `Basic ${basic}`}});
 		if (!response.ok()) {
 			console.log('response status:', response.status());
 			console.log(await response.text());
-            console.log('basic auth header:', `Basic ${basic}`);
-            console.log('HEADERS SENT:', {Authorization: `Basic ${basic}`}
-);
-
 		}
 		expect(response.ok()).toBeTruthy();
 		const body = await response.json();
